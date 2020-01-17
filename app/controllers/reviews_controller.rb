93f314d432b7be47ccf38e9f0ababe08b0256ddd
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 11:24:28 -0800

    debug issues with update and views, functional

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,7 @@ class ReviewsController < ApplicationController
         @product = Product.find(params[:product_id])
         @review = @product.reviews.new(review_params)
         if @review.save
+            flash[:notice] = "#{@product.name} has been reviewed!"
             redirect_to product_path(@product)
         else
             render :new
@@ -18,6 +19,22 @@ class ReviewsController < ApplicationController
         @product = Product.find(params[:product_id])
         @review = Review.find(params[:id])
         render :show        
+    end
+    
+    def edit
+        @product = Product.find(params[:product_id])
+        @review = Review.find(params[:id])
+        render :edit
+    end
+    
+    def update
+        @review = Review.find(params[:id])
+        if @review.update(review_params)
+          flash[:notice] = "#{@review.author}'s review has been updated!"
+          redirect_to product_path(@review.product)
+        else
+          render :edit
+        end
     end
     
     private
