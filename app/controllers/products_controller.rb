3520fa60d6bd2baf9e82dc8eed76dc72913205fd
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 14:11:23 -0800

    add homepage, reroute, reroot

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
 
+    def home_page
+        @most_reviewed_product = Product.product_with_most_reviews
+        @made_in_usa = Product.made_in_usa
+        @recently_added = Product.recently_added
+        render :home_page
+    end
+
     # Code for listing all products
     def index
         @products = Product.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  root to: 'products#home_page'
   resources :products do
     resources :reviews
   end
