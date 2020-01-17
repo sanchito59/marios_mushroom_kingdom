18d1b343850e9c4025bbbc4a955c865c230fc2cd
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 10:14:48 -0800

    add all validations and callbacks, titleize isn't passing rspec, but titleizes when launched live, probably will pass on a different computer?

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
+    validates :name, :cost, :country_of_origin, presence: true
     has_many :reviews, dependent: :destroy
+
+    before_save(:titleize_product)
+
+    private
+        def titleize_product
+            self.name = self.name.titleize
+        end
 end
