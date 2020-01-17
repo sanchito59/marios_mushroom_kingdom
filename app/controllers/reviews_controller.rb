7b0469191383f684f60969d5cf32039364877fd6
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 11:04:17 -0800

    add ability to create a review and show it, specs still pasing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,25 @@ class ReviewsController < ApplicationController
         @product = Product.find(params[:product_id])
         @review = @product.reviews.new 
     end
+    
+    def create
+        @product = Product.find(params[:product_id])
+        @review = @product.reviews.new(review_params)
+        if @review.save
+            redirect_to product_path(@product)
+        else
+            render :new
+        end
+    end
+    
+    def show
+        @product = Product.find(params[:product_id])
+        @review = Review.find(params[:id])
+        render :show        
+    end
+    
+    private
+        def review_params
+            params.require(:review).permit(:author, :content_body, :rating)
+        end
 end
