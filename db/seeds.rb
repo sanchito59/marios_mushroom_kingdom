9c44cb52410c7132461cb7c610e9913341aa3cb8
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 11:58:29 -0800

    seed database with 50-125 products and 3-15 reviews for each depending on how many products are randomly generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+rand(50..125).times do |index|
+  products = []
+  products.push(Product.create!(name: Faker::Appliance.equipment,
+    cost: Faker::Commerce.price,
+    country_of_origin:  Faker::Address.country))
+  products
+  rand(3..15).times do |review|
+    products.each do |product|
+    Review.create! :author => Faker::TvShows::Simpsons.character, :content_body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue faucibus urna at dignissim. Nunc gravida convallis ullamcorper. Donec sit.", :rating => rand(1..5),
+                     :product_id => product.id
+    end
+  end
+end
+
+p "Created #{Product.count} products with #{Review.count} reviews"
