2bf2c7e1cc019659eccf5a1183c57c3170394e33
Chris Sanchez <c.sanch7@gmail.com>
2020-01-17 13:03:23 -0800

    add ability to sort by three most recently added products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     scope :product_with_most_reviews, -> { }
     scope :made_in_usa, -> { where(country_of_origin: "United States of America") }
-    scope :recently_added, -> { }
+    scope :recently_added, -> { order(created_at: :desc).limit(3)}
 
     has_many :reviews, dependent: :destroy
     validates :name, :cost, :country_of_origin, presence: true
